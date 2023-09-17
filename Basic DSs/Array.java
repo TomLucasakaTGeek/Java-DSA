d8a7dbff3bd418fac5585fb719b2180332fb1a09
Tanmay Khanna <120933239+TomLucasakaTGeek@users.noreply.github.com>
2023-09-17 22:08:17 +0530

    Update Array.java

--- Basic DSs/Array.java
+++ Basic DSs/Array.java
@@ -1,1 +1,12 @@
+import java.util.*;
+class Arr {
+  public static void main(String args[]) {
+    Scanner o = new Scanner(System.in);
+    int arr = new arr[10];
+    for(int i=0;i<10;i++)
+      arr  = o.nextInt();
 
+    for(int i=0;i<10;i++)
+      System.out.println(arr[i]);
+  }
+}
