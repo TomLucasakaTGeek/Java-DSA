b63914ddc82c2c03901c315da788d20d144f120a
Tanmay Khanna <120933239+TomLucasakaTGeek@users.noreply.github.com>
2023-09-18 20:28:38 +0530

    Update LinkedList.java

--- Basic DSs/LinkedList.java
+++ Basic DSs/LinkedList.java
@@ -1,1 +1,22 @@
-
+import java.util.*;
+class Links {
+  public static void main(String args[]) {
+    LinkedList<String> list = new LinkedList<String>();
+//adding elements on First
+    list.addFirst("a");
+    list.addFirdt("is");
+//adding elements in Last
+    list.addLast("this");
+    list.add("list");
+//iterating over list
+  for(int i=0;i<list.size();i++) {
+    System.out.println(list.get(i)+"->");
+  }System.out.println("null");
+//remove first element
+    list.removeFirst();
+//remove last element
+    list.removeLast();
+//printing LinkedList
+    System.out.println(list);
+//size of LinkedList
+    System.out.println(list.size());  
