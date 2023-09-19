4ba31357d6d728212a1d0873b799d4cd6d4f56a6
Tanmay Khanna <120933239+TomLucasakaTGeek@users.noreply.github.com>
2023-09-19 19:54:29 +0530

    Update String.java

--- Basic DSs/String.java
+++ Basic DSs/String.java
@@ -3,4 +3,10 @@ import java.util.*;
 class ObjOfChars {
   public static void main(String args[]) {
     Scanner o = new Scanner(System.in);
-    
+    System.out.println("Enter text");
+    String s = o.nextLine();   // o.next() can be used too
+
+    for(int i=0;i<s.length();i++)
+      System.out.println(s.charAt(i));
+  }
+}
